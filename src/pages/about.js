d16ca34a9e19cd3ccc5d0c6de95ff4690b8eb767
Shubham Mani Tripathi <mani9k9@gmail.com>
2023-07-01 17:30:39 +0530

    about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,18 @@
+import AnimatedText from '@/components/AnimatedText'
 import React from 'react'
+import Head from 'next/head'
 
 const about = () => {
   return (
-    <div>about</div>
+    <>
+    <Head>
+      <title>Shubham | About Page</title>
+      <meta name="description" content="any description" />
+    </Head>
+    <main>
+      <AnimatedText text="Passion Fuels Purpose! " />
+    </main>
+    </>
   )
 }
 

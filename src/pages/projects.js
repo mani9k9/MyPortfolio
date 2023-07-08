5427d71dc1aca2fa521127d2df89e9f5119c319f
Shubham Mani Tripathi <mani9k9@gmail.com>
2023-07-08 20:42:10 +0530

    responsive project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -40,22 +40,22 @@ import project5 from "../../public/images/projects/portfolio.jpg"
 const Project = ({ title, type, img, link, github }) => {
   return (
     <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark
-     bg-light p-6 relative dark:bg-dark dark:border-light'>
-      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light' />
+     bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4'>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]' />
       <Link href={link} target='_blank'
         className='w-full cursor-pointer overflow-hidden rounded-lg'
       >
         <Image src={img} alt={title} className="w-full h-auto" />
       </Link>
       <div className='w-full flex flex-col items-start justify-between mt-4 '>
-        <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base'>{type}</span>
         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-3xl font-bold '>{title}</h2>
+          <h2 className='my-2 w-full text-left text-3xl font-bold lg:text-2xl '>{title}</h2>
         </Link>
 
         <div className='w-full mt-2 flex items-center justify-between'>
-          <Link href={link} target='_blank' className='rounded-lg font-semibold underline'>Visit</Link>
-          <Link href={github} target='_blank' className='w-8'><GithubIcon />{" "}</Link>
+          <Link href={link} target='_blank' className='rounded-lg font-semibold underline md:text-base'>Visit</Link>
+          <Link href={github} target='_blank' className='w-8 md:w-6'><GithubIcon />{" "}</Link>
         </div>
       </div>
 
@@ -74,12 +74,12 @@ const projects = () => {
       <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
           <AnimatedText text="Imagination Trumps Knowledge!"
-            className='mb-16'
+            className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'
           />
 
-          <div className='grid grid-cols-12 gap-24 gap-y-32'>
+          <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
 
-          <div className='col-span-6'>
+          <div className='col-span-6 sm:col-span-12'>
               <Project
                 title="Portfolio"
                 img={project5}
@@ -88,7 +88,7 @@ const projects = () => {
                 type="Featured Project"
               />
             </div>
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                 title="Blog App"
                 img={project1}
@@ -97,7 +97,7 @@ const projects = () => {
                 type="Featured Project"
               />
             </div>
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                 title="Car Show"
                 img={project2}
@@ -106,7 +106,7 @@ const projects = () => {
                 type="Featured Project"
               />
             </div>
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                 title="Big Boss Running"
                 img={project3}
@@ -115,7 +115,7 @@ const projects = () => {
                 type="Featured Project"
               />
             </div>
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                 title="Peolpe's Talk"
                 img={project4}
